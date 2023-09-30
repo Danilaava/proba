0d1232e7fc16b137e04fde6d542a91e4631da0c3
Danilaava <144874842+Danilaava@users.noreply.github.com>
2023-09-30 17:42:15 +0500

    Program.cs

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -14,4 +14,4 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int square = number * number;
 Console.WriteLine(number + " -> " + square);
-Console.WriteLine($"{number} -> {square}");
+//Console.WriteLine($"{number} -> {square}"); --интерполяция, типа удобнее
