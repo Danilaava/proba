d4312ff546d32c0c6b179986e09699366a3f0b4d
Danilaava <belyakov_11_11@mail.ru>
2023-10-04 12:41:27 +0500

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int number = ReadInt("Введите трёхзначное число: ");
+Console.WriteLine(InCenter(number));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
